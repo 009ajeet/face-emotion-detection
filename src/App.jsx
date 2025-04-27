dbd7b0f1d1e0ff9c5cdb272ed84725d1fc0aef31
Ajeet Singh <as7641012@gmail.com>
2025-04-27 16:30:37 +0530

    fixed new bugs

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
   useEffect(() => {
     const loadModels = async () => {
       await Promise.all([
-        faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-        faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-        faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-        faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+        faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+        faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+        faceapi.nets.faceExpressionNet.loadFromUri('./models'),
       ]);
       startVideo();
     };
