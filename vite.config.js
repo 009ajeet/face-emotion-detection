28ae11ba22fe1fec50117392ecbc9cc5c62dea5f
Ajeet Singh <as7641012@gmail.com>
2025-04-27 16:00:44 +0530

    H

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // or whatever framework you're using
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/face-emotion-detection/', // Match your repository name
+  build: {
+    outDir: 'dist',
+  }
 })
